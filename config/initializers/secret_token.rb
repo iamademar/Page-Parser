34ceae199a469b759332ad9eb572c5af0d7502e2
Ademar Tutor <ademartutor@Ademar-Tutors-MacBook-Pro.local>
2011-09-26 11:16:13 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PageParser::Application.config.secret_token = '839b08c169327b9f7bd4cb572477546be0dffe0f1619b0fac90c561bbada0c1fbf7e0481fa6122060bf56f60c1b50e3e205557b635cb98f97c3842d438722e56'
